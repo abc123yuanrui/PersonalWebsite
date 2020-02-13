21d3dfa0c0c532c74061399a4fb9a509c397c596
Your Name <you@example.com>
2020-02-13 22:23:46 +1100

    feat:build

--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,13 @@ class HomePage extends Component {
             <Header />
             <div>
 <h2 className="myH2">About me ?</h2>
-<p className="myP">This is a responsive fixed navbar animated on scroll</p>
-<p className="myP">I took inspiration from  ABDO STEIF (<a href="https://codepen.io/abdosteif/pen/bRoyMb?editors=1100">https://codepen.io/abdosteif/pen/bRoyMb?editors=1100</a>)
-and Dicson <a href="https://codepen.io/dicson/pen/waKPgQ">(https://codepen.io/dicson/pen/waKPgQ)</a></p>
+<p className="myP">I am Ray</p>
+<p className="myP">My project sources can be found on (<a href="https://github.com/abc123yuanrui">My Github Profile</a> </p>
 <p className="myP">I HOPE YOU FIND THIS USEFULL</p>
-<p className="myP">Albi</p>
-<p className="myP">				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+<p className="myP">Rui (Ray) Yuan</p>
+<p className="myP">	My contact info:
 </p>
-<p className="myP">Albi</p>
-<p className="myP">Albi</p>
-<p className="myP">Albi</p>
-<p className="myP">Albi</p>
-<p className="myP">Albi</p>
 </div>
-            <p>Welcome to Next.js!</p>
             <ReactPlayer className="myVideo" url= {thisUrl} playing= {true}  controls= {true}/>
             <img className="myPic" src='https://drive.google.com/uc?id=0Bwezf3fxXVZkakF3Rnl1dkxvR3FrM3Zlekd0UVZLYU5KQ1Rz'/>
             </div>
