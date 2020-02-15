1f1e5c70bd4958714ff0450e47dd7d4d077c9072
Your Name <you@example.com>
2020-02-15 18:56:12 +1100

    feat: footer

---
+++ components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <div class="footer">
+        <div id="button"></div>
+      <div id="container">
+      <div id="cont">
+      <div class="footer_center">
+             <h3>Contact me:</h3>
+             <a href="https://www.linkedin.com/in/rui-yuan-5953aa168/"><img className="myContactLogo" src='/icons/linkedin.png'/></a>
+             <a href="https://github.com/abc123yuanrui"><img className="myContactLogo" src='/icons/github.png'/></a>
+             <a href="https://www.facebook.com/profile.php?id=100013714845427"><img className="myContactLogo" src='/icons/fb.png'/></a>
+      </div>
+      </div>
+      </div>
+      </div>
+    );
+  };
+  
+export default Footer;
