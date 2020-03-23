49faf11cfb06a010690d6b2ea6266a457b6a361a
Your Name <you@example.com>
2020-03-23 11:28:43 +1100

    feat: Image size processing

--- pages/service.js
+++ pages/service.js
@@ -8,7 +8,7 @@ export default class Service extends Component {
             <div>
                 <Navbar />
                 <div className='serviceTitle'>Add cartoon effect to your photo</div>
-                <div className='serviceP'>Choose a photo to process and see the difference! (It may take few seconds to few minutes depending on the size of your photo)</div>
+                <div className='serviceP'>Choose a photo to process and see the difference! (It may take few seconds to few minutes depending on the quality of your photo)</div>
                 <Uploader />
                 <Footer />
             </div>
