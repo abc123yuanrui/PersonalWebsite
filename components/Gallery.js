a8cb77b835e4c87c2f7edc6f8e0f86b4e2f2d16c
Rui Yuan <123abcyuanrui@gmail.com>
2021-11-10 11:52:39 +1030

    update: photos

--- components/Gallery.js
+++ components/Gallery.js
@@ -19,8 +19,8 @@ const gallery = [
     description: "Night reflection"
   },
   {
-    url:"https://live.staticflickr.com/65535/50889587191_f3dd3fc7ad_k_d.jpg",
-    description: "Sun set at Adelaide beach-1"
+    url:"https://live.staticflickr.com/65535/51668503876_29fa4dc37b_k_d.jpg",
+    description: "Immersion show"
   },
   {
     url:"https://live.staticflickr.com/65535/50888874458_afbc012999_k_d.jpg",
@@ -31,8 +31,8 @@ const gallery = [
     description: "A lane near my home"
   },
   {
-    url:"https://live.staticflickr.com/65535/49625098788_d7ac38f7d5_b.jpg",
-    description: "Sun set at Flagstaff"
+    url:"https://live.staticflickr.com/65535/51668506786_cbc6c1e891_k_d.jpg",
+    description: "AU Asian Festivel lantern show"
   },
   {
     url:"https://live.staticflickr.com/65535/49641503718_f44c513261_k.jpg",
